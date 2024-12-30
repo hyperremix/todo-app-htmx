44e3b36620a00d6678b9043b359a726d2c290bbc
Fredrik Pettersson <fredrik.pettersson.89@gmail.com>
2024-12-30 12:48:42 +0100

    Change layout to better accomodate larger screens

---
+++ util/class.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"strings"
+
+	"github.com/a-h/templ"
+)
+
+func GetClassAttributeValue(attributes templ.Attributes) string {
+	if class, ok := attributes["class"].(string); ok {
+		return class
+	}
+
+	return ""
+}
+
+func GetClassAttributeSlice(attributes templ.Attributes) []string {
+	if class, ok := attributes["class"].(string); ok {
+		return strings.Split(class, " ")
+	}
+
+	return []string{}
+}
